4b156538956d37fdf088bd560dd18184f8436748
hooks_28 <1r56955415918@qq.com>
2023-12-14 14:13:32 +0800

    :sparkles:feat(all): new monorepo repo and add packages(utils)

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: @memo28.repo
+ * @Date: 2023-12-14 14:11:36
+ * @LastEditTime: 2023-12-14 14:12:09
+ * @Description: 
+ * @FilePath: /businessRepo/packages/utils/src/index.ts
+ */
+export { };
